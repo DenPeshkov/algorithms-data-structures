c4d069bb9b60904da4d95986094dbbc9aab71f84
Denis Peshkov <denpeshkov98@gmail.com>
2022-06-10 15:15:22 +0300

    Added docs to BinaryMergeInPlaceFunction and SortingFunction
    Signed-off-by: Denis Peshkov <denpeshkov98@gmail.com>

--- lib/src/main/java/com/github/denpeshkov/algorithms/merge/binary/inplace/BinaryMergeInPlaceFunction.java
+++ lib/src/main/java/com/github/denpeshkov/algorithms/merge/binary/inplace/BinaryMergeInPlaceFunction.java
@@ -3,5 +3,15 @@ package com.github.denpeshkov.algorithms.merge.binary.inplace;
 @FunctionalInterface
 public interface BinaryMergeInPlaceFunction<T extends Comparable<? super T>> {
 
+  /**
+   * Merges two consecutive sorted ranges [{@code first}, {@code middle}) and [{@code middle},
+   * {@code last}) into one sorted range [{@code first}, {@code last})
+   *
+   * @param arr the array with the elements to be merged
+   * @param first the beginning of the first sorted range, inclusive
+   * @param middle the end of the first sorted range, exclusive, and the beginning of the second,
+   *     inclusive
+   * @param last the end of the second sorted range, exclusive
+   */
   void merge(T[] arr, int first, int middle, int last);
 }
--- lib/src/main/java/com/github/denpeshkov/algorithms/sorting/SortingFunction.java
+++ lib/src/main/java/com/github/denpeshkov/algorithms/sorting/SortingFunction.java
@@ -3,10 +3,23 @@ package com.github.denpeshkov.algorithms.sorting;
 @FunctionalInterface
 public interface SortingFunction<T extends Comparable<? super T>> {
 
+  /**
+   * Sorts the specified array into non-descending order
+   *
+   * @param arr the array to be sorted
+   */
   default void sort(T[] arr) {
     sort(arr, 0, arr.length);
   }
 
+  /**
+   * Sorts the elements in the range [{@code first}, {@code last}) of the array into non-descending
+   * order
+   *
+   * @param arr the array with the elements to be sorted
+   * @param from the index of the first element, inclusive, to be sorted
+   * @param to the index of the last element, exclusive, to be sorted
+   */
   void sort(T[] arr, int from, int to);
 
   // TODO add sort for List
