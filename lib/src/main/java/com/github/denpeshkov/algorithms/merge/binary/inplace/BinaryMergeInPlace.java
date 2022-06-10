5f8c9fdcf1220dad13b2fde08388040049d2f319
Denis Peshkov <denpeshkov98@gmail.com>
2022-06-10 16:44:34 +0300

    Added docs to BinaryMergeInPlace Refactored parameters names
    Signed-off-by: Denis Peshkov <denpeshkov98@gmail.com>

--- lib/src/main/java/com/github/denpeshkov/algorithms/merge/binary/inplace/BinaryMergeInPlace.java
+++ lib/src/main/java/com/github/denpeshkov/algorithms/merge/binary/inplace/BinaryMergeInPlace.java
@@ -15,17 +15,28 @@ public class BinaryMergeInPlace<T extends Comparable<? super T>>
     merge(arr, first, middle, last, aux);
   }
 
-  public void merge(T[] arr, int lo, int mid, int hi, T[] aux) {
-    System.arraycopy(arr, lo, aux, lo, hi - lo);
+  /**
+   * Merges two consecutive sorted ranges [{@code first}, {@code middle}) and [{@code middle},
+   * {@code last}) into one sorted range [{@code first}, {@code last})
+   *
+   * @param arr the array with the elements to be merged
+   * @param first the beginning of the first sorted range, inclusive
+   * @param middle the end of the first sorted range, exclusive, and the beginning of the second,
+   *     inclusive
+   * @param last the end of the second sorted range, exclusive
+   * @param aux the auxiliary array to store the elements in the range [{@code first}, {@code last})
+   */
+  public void merge(T[] arr, int first, int middle, int last, T[] aux) {
+    System.arraycopy(arr, first, aux, first, last - first);
 
-    int i = lo;
-    int j = mid;
+    int i = first;
+    int j = middle;
 
-    for (int k = lo; k < hi; k++) {
+    for (int k = first; k < last; k++) {
       // <= is used to ensure stability
-      if (j >= hi || lessOrEqual(aux[i], aux[j])) {
+      if (j >= last || lessOrEqual(aux[i], aux[j])) {
         arr[k] = aux[i++];
-      } else if (i >= mid || greater(aux[i], aux[j])) {
+      } else if (i >= middle || greater(aux[i], aux[j])) {
         arr[k] = aux[j++];
       }
     }
