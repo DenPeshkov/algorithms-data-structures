00551484c17398bbc2290eca238f6cb67ca187ed
Denis Peshkov <denpeshkov98@gmail.com>
2022-06-14 16:51:56 +0300

    Added Improved Bottom-up Mergesort
    Signed-off-by: Denis Peshkov <denpeshkov98@gmail.com>

---
+++ lib/src/main/java/com/github/denpeshkov/algorithms/MergeSortBottomUpImproved.java
@@ -0,0 +1,34 @@
+package com.github.denpeshkov.algorithms;
+
+import static com.github.denpeshkov.utils.ComparableUtils.lessOrEqual;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@SuppressWarnings("SameNameButDifferent")
+public class MergeSortBottomUpImproved<T extends Comparable<? super T>>
+    implements SortingFunction<T> {
+
+  private final BinaryMergeInPlaceImproved<T> binaryMergeInPlaceImproved;
+
+  @Override
+  public void sort(T[] arr, int from, int to) {
+    int N = arr.length;
+
+    @SuppressWarnings("unchecked")
+    T[] aux = (T[]) new Comparable[N];
+
+    for (int len = 1; len < N; len *= 2) {
+      for (int first = 0; first < N - len; first += 2 * len) {
+        int middle = first + len;
+        int last = Math.min(first + (2 * len), N);
+
+        if (lessOrEqual(arr[middle - 1], arr[middle])) {
+          continue;
+        }
+
+        binaryMergeInPlaceImproved.merge(arr, first, middle, last, aux);
+      }
+    }
+  }
+}
