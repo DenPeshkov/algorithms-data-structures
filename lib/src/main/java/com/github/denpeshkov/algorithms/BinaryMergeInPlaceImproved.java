2ef3d0f5dcb2d57ecc7225c4b6f3d04b4bf252b2
Denis Peshkov <denpeshkov98@gmail.com>
2022-06-14 16:51:29 +0300

    Added Improved Binary in-place Merge
    Signed-off-by: Denis Peshkov <denpeshkov98@gmail.com>

---
+++ lib/src/main/java/com/github/denpeshkov/algorithms/BinaryMergeInPlaceImproved.java
@@ -0,0 +1,34 @@
+package com.github.denpeshkov.algorithms;
+
+import static com.github.denpeshkov.utils.ComparableUtils.less;
+
+public class BinaryMergeInPlaceImproved<T extends Comparable<? super T>>
+    implements BinaryMergeInPlaceFunction<T> {
+
+  @Override
+  public void merge(T[] arr, int first, int middle, int last) {
+    @SuppressWarnings("unchecked")
+    T[] aux = (T[]) new Comparable[middle - first];
+
+    merge(arr, first, middle, last, aux);
+  }
+
+  public void merge(T[] arr, int first, int middle, int last, T[] aux) {
+    System.arraycopy(arr, first, aux, 0, middle - first);
+
+    int i = 0;
+    int j = middle;
+
+    for (int k = first; k < last; k++) {
+      if (i >= middle - first) {
+        break;
+      } else if (j >= last) {
+        arr[k] = aux[i++];
+      } else if (less(arr[j], aux[i])) {
+        arr[k] = arr[j++];
+      } else {
+        arr[k] = aux[i++];
+      }
+    }
+  }
+}
